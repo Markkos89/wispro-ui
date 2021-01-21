42d840e055309ad2166f3a7125652824d0cd4ecf
Agustin Veloso <mauro.alejandro.perez@outlook.es>
2021-01-21 14:37:02 -0300

    correcciones de css

--- src/components/PrivateRoute/PrivateRoutes.js
+++ src/components/PrivateRoute/PrivateRoutes.js
@@ -1,5 +1,6 @@
 import NavBar from "components/NavBar/NavBar";
 import React from "react";
+import { Container } from "react-bootstrap";
 import { connect } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
 
@@ -7,16 +8,18 @@ const PrivateRoute = ({ component: Component, loggedIn, ...rest }) => {
   return (
     <React.Fragment>
       <NavBar />
-      <Route
-        {...rest}
-        render={(props) =>
-          JSON.parse(localStorage.getItem("user"))?.tokens?.access?.token ? (
-            <Component {...props} />
-          ) : (
-            <Redirect to="/" />
-          )
-        }
-      />
+      <Container>
+        <Route
+          {...rest}
+          render={(props) =>
+            JSON.parse(localStorage.getItem("user"))?.tokens?.access?.token ? (
+              <Component {...props} />
+            ) : (
+              <Redirect to="/" />
+            )
+          }
+        />
+      </Container>
     </React.Fragment>
   );
 };
