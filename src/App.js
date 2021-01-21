27086ebb094f28f4b8c522acee4cab750316e68e
Agustin Veloso <mauro.alejandro.perez@outlook.es>
2021-01-21 14:49:11 -0300

    agrega bootstrap como una dependencia

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-
 import { RecoilRoot } from 'recoil';
-
 import withErrorHandler from 'errorHandling';
 import { App as ErrorBoundaryFallback } from 'errorHandling/Fallbacks';
-
 import { Route, Switch } from 'react-router-dom'
 import { privateRoutes, publicRoutes } from 'routes'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux'
-
 import { store } from './redux/store'
-
 import PrivateRoute from './components/PrivateRoute/PrivateRoutes'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
